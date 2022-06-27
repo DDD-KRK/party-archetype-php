841fd8c8219f23c6a6cb042f73cb741d430c833a
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2022-06-27 18:07:16 +0200

    init

---
+++ src/Party.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archetype\Party;
+
+class Party
+{
+    public function getIdentifier(): PartyIdentifier
+    {
+
+    }
+    /**
+     * @return RegisteredIdentifier[]
+     */
+    public function getRegisteredIdentifiers() : array
+    {
+
+    }
+
+    public function getName() : string
+    {
+        
+    }
+
+}
---
+++ tests/PartyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archetype\Party\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PartyTest extends TestCase
+{
+}
